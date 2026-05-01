7287d38086a7f4e55ccb2ab74665beb96f6e58dc
Prosper Shaibu Asare <asareprosper143@gmail.com>
2026-05-01 10:14:56 +0000

    feat: add animated welcome screen with authentication navigation flow

--- mindbridge-mobile/app/(auth)/welcome.tsx
+++ mindbridge-mobile/app/(auth)/welcome.tsx
@@ -10,11 +10,7 @@ export default function WelcomeScreen() {
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Animated.View entering={FadeIn.duration(1000).delay(300)} style={styles.iconContainer}>
-          <Image 
-            source={require('../../assets/images/logo.png')} 
-            style={styles.logo} 
-            resizeMode="contain"
-          />
+          <View style={styles.orb} />
         </Animated.View>
 
         <View style={styles.textContainer}>
